4f245db172fc69544a97e5944558f8094625d626
Pubudu Janith <pubudujanith123@gmail.com>
2022-12-30 18:25:47 +0530

    Complete database connection logic

--- src/main/java/lk/ijse/dep9/orm/InitializeDB.java
+++ src/main/java/lk/ijse/dep9/orm/InitializeDB.java
@@ -1,5 +1,32 @@
 package lk.ijse.dep9.orm;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class InitializeDB {
 
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void initialize(String host,
+                                  String port,
+                                  String database,
+                                  String username,
+                                  String password,
+                                  String ... packageToScan) {
+        String url = "jdbc:mysql://%s:%s/%s?createDatabaseIfNotExist=true";
+        url = String.format(url, host, port, database);
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(url,username,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
