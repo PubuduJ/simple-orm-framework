1c33116933029aad098d2c77a24825c432f73154
Pubudu Janith <pubudujanith123@gmail.com>
2022-12-30 18:45:38 +0530

    Complete packages scan logic

--- src/main/java/lk/ijse/dep9/orm/InitializeDB.java
+++ src/main/java/lk/ijse/dep9/orm/InitializeDB.java
@@ -1,8 +1,13 @@
 package lk.ijse.dep9.orm;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InitializeDB {
 
@@ -19,14 +24,33 @@ public class InitializeDB {
                                   String database,
                                   String username,
                                   String password,
-                                  String ... packageToScan) {
+                                  String ... packagesToScan) {
         String url = "jdbc:mysql://%s:%s/%s?createDatabaseIfNotExist=true";
         url = String.format(url, host, port, database);
         Connection connection;
         try {
             connection = DriverManager.getConnection(url,username,password);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e.getMessage());
         }
+
+        List<String> classNames = new ArrayList<>();
+
+        for (int i = 0; i < packagesToScan.length; i++) {
+            System.out.println(packagesToScan[i]);
+            String entityPackage = packagesToScan[i].replaceAll("\\.", "/");
+            System.out.println(entityPackage);
+            URL resource = InitializeDB.class.getResource("/".concat(entityPackage));
+            try {
+                File file = new File(resource.toURI());
+                String[] list = file.list();
+                for (String name : list) {
+                    classNames.add(packagesToScan[0].concat(".").concat(name.replace(".class","")));
+                }
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        classNames.forEach(System.out::println);
     }
 }
